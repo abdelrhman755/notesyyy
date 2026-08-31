f3806d2e1119beaa704e1d4c4d5e384415decd11
Abdelrhman <abdelrazkforman966@gmail.com>
2026-08-31 13:57:46 +0300

    start working on the ui of the plugin i added a title, subtitle and an input field in the dock

--- addons/notesyyy/notesyy.gd
+++ addons/notesyyy/notesyy.gd
@@ -2,14 +2,45 @@
 extends EditorPlugin
 
 var ui : Control
+var container: VBoxContainer
+var button: Button
+var line: LineEdit
+
 func _enter_tree():
+	#making the dock 
 	ui = Control.new()
 	ui.name = "Notesyyy"
-	add_control_to_dock(DOCK_SLOT_LEFT_UL, ui)
 	ui.custom_minimum_size = Vector2(250 , 400)
-
-
+	
+	# the container of all the elements in the dock
+	var vbox = VBoxContainer.new()
+	vbox.set_anchors_preset(Control.PRESET_FULL_RECT)
+	ui.add_child(vbox)
+	
+	
+	# title of the dock
+	var title = Label.new()
+	title.text = " Welcome To Notesyyy"
+	title.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	vbox.add_child(title)
+	
+	# subtitile
+	var subt = Label.new()
+	subt.text = "Let's manage your project..\n"
+	subt.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	vbox.add_child(subt) 
+	
+	# the input box 
+	line = LineEdit.new()
+	line.placeholder_text = "Write a Note..."
+	vbox.add_child(line)
+	line.text_submitted.connect(add_note)
+	
+	add_control_to_dock(DOCK_SLOT_LEFT_UL, ui)
 
 func _exit_tree():
 	remove_control_from_docks(ui)
 	ui.free()
+
+func add_note():
+	pass
